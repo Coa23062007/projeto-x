96c3bffddb5dd8455de749f081a845e7b7130e79
Coa23062007 <00001074925671sp@al.educacao.sp.gov.br>
2024-12-04 08:19:21 -0300

    Add files via upload

---
+++ Nova pasta/alraflix.pt3/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'qual é o melhor time da história?',
+    'O melhor time é o (corinthians)'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Bahia?',
+    'A capital da Bahia é (Salvador)'
+)
+
+criaCartao(
+    'Escola',
+    'quantos continentes existem?',
+    'A resposta certa é (6, África, América, Antártida, Ásia, Europa e Oceania)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz qua o seu nome em Inglês?',
+    'Oi em ingles é (what is your name)'
+)
